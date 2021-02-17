9fd1eb7708e8239b550e700901123db0d5bd85ff
marcvaldiviaprim <marcvaldiviaprim99@gmail.com>
2021-02-17 16:30:18 +0100

    Add components

--- README.md
+++
@@ -1,2 +0,0 @@
-# 2Geek.com
-Ilerna - Proyecto de final de curso de DAW2 (2020/2021)
--- TwoGeek/src/app/app.module.ts
+++ TwoGeek/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComentariosComponent } from './comentarios/comentarios.component';
+import { ComunidadComponent } from './comunidad/comunidad.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { PaginaInicioComponent } from './pagina-inicio/pagina-inicio.component';
+import { PerfilMainComponent } from './perfil-main/perfil-main.component';
+import { PerfilUsuarioComponent } from './perfil-usuario/perfil-usuario.component';
+import { PostComponent } from './post/post.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComentariosComponent,
+    ComunidadComponent,
+    HeaderComponent,
+    LoginComponent,
+    PaginaInicioComponent,
+    PerfilMainComponent,
+    PerfilUsuarioComponent,
+    PostComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
