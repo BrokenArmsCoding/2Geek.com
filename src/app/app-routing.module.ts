18768a29945117bab074501743a05ceea433b21d
SergiPaz17 <72407380+SergiPaz17@users.noreply.github.com>
2021-04-09 23:37:40 +0200

    UwU v4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'Login'},
+  { path: 'Login', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
