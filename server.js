1162a931d29a2c7350e3a94f504f5b4d22ce4e15
SergiPaz17 <72407380+SergiPaz17@users.noreply.github.com>
2021-04-10 00:15:33 +0200

    UwU v5

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static('./src/'));
+app.use(express.static('./dist/TwoGeek'));
 app.get('/*', (req, res) =>
- res.sendFile('index.html', {root: './src/'}),
+ res.sendFile('index.html', {root: './dist/TwoGeek'}),
 );
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ComentariosComponent } from './Components/comentarios/comentarios.component';
+import { ComunidadComponent } from './Components/comunidad/comunidad.component';
+import { HeaderComponent } from './Components/header/header.component';
 import { LoginComponent } from './Components/login/login.component';
+import { PaginaInicioComponent } from './Components/pagina-inicio/pagina-inicio.component';
+import { PerfilMainComponent } from './Components/perfil-main/perfil-main.component';
+import { PerfilUsuarioComponent } from './Components/perfil-usuario/perfil-usuario.component';
+import { PostComponent } from './Components/post/post.component';
+import { RegistroComponent } from './Components/registro/registro.component';
+
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'Login'},
-  { path: 'Login', component: LoginComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'comentarios', component: ComentariosComponent},
+  { path: 'comunidad', component: ComunidadComponent},
+  { path: 'header', component: HeaderComponent},
+  { path: 'PaginaInicio', component: PaginaInicioComponent},
+  { path: 'PerfilMain', component: PerfilMainComponent},
+  { path: 'PerfilUsuario', component: PerfilUsuarioComponent},
+  { path: 'Post', component: PostComponent},
+  { path: 'Registro', component: RegistroComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
