9152e2d0f531ff5b05a273baa275c86b68170a72
Jagger_without_ACOG <57674219+Angry-Jagger-Without-ACOG@users.noreply.github.com>
2021-05-16 17:37:27 +0200

    formulario para crear post hecho

--- src/app/Components/comunidad/perfil-comunidad/perfil-comunidad.component.ts
+++ src/app/Components/comunidad/perfil-comunidad/perfil-comunidad.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ServicesService } from 'src/app/Services/services.service';
 
 @Component({
@@ -8,22 +9,40 @@ import { ServicesService } from 'src/app/Services/services.service';
 })
 export class PerfilComunidadComponent implements OnInit {
 
+  ModoCambio: String = "Posts";
   nombreComunidad: String;
   descripcionComunidad: String;
   NombreUsuario: String;
+  NuevoPost: FormGroup;
 
-  constructor(private BD: ServicesService) { }
+  constructor(private formBuilder: FormBuilder, private BD: ServicesService) { }
 
   ngOnInit(): void {
 
     this.nombreComunidad =this.BD.getDescripcionComunidad();
     this.descripcionComunidad = this.BD.getNombreComunidad();
-
     this.NombreUsuario = localStorage.getItem("User");
 
     console.log(this.nombreComunidad,this.descripcionComunidad);
 
+    this.NuevoPost = this.formBuilder.group({
+      titulo: ['', Validators.required],
+      texto: ['', Validators.required]
+    });
+
   }
 
+  Cambiar_Opcion(op: String): void {
+    this.ModoCambio = op;
+  }
+
+  Volver(): void {
+    this.NuevoPost.reset();
+    this.Cambiar_Opcion("Posts");
+  }
+
+  get infopost() {
+    return this.NuevoPost.controls;
+  }
 
 }
