71e4d7e8c2381f17e82c7630f7265c5ef279e091
Jagger_without_ACOG <57674219+Angry-Jagger-Without-ACOG@users.noreply.github.com>
2021-05-17 19:27:43 +0200

    Update perfil-comunidad.component.ts

--- src/app/Components/comunidad/perfil-comunidad/perfil-comunidad.component.ts
+++ src/app/Components/comunidad/perfil-comunidad/perfil-comunidad.component.ts
@@ -14,7 +14,7 @@ export class PerfilComunidadComponent implements OnInit {
 
   NuevoPost: FormGroup;
 
-  Permisos:Number = 4;
+  Permisos:Number = 1;
   ModoCambio: String = "Posts";
   nombreComunidad: String;
   descripcionComunidad: String;
